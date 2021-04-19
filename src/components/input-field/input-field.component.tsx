1b6eb94dc1befac0e3fb4496eb344ecb8cf74a26
daniil-kh <daniil.khmurovich@gmail.com>
2021-04-19 16:24:33 +0300

    add input component

---
+++ src/components/input-field/input-field.component.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import InputFieldContainer, { TextInput } from "./input-field.styled";
+
+interface InputFieldProps {
+  onChangeText: React.ChangeEventHandler;
+  value?: string;
+}
+
+const InputField: React.FC<InputFieldProps> = ({
+  onChangeText,
+  value,
+}): JSX.Element => {
+  return (
+    <InputFieldContainer>
+      <TextInput value={value} onChange={onChangeText} />
+    </InputFieldContainer>
+  );
+};
+
+export default InputField;
---
+++ src/components/input-field/input-field.styled.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const InputFieldContainer = styled.div`
+  padding: 5px;
+`;
+
+export const TextInput = styled.input`
+  height: 100%;
+`;
+
+export default InputFieldContainer;
