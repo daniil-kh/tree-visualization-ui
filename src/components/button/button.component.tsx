64e96edd2cd25834dcb7c2488cf45c76fd8794e5
daniil-kh <daniil.khmurovich@gmail.com>
2021-04-19 16:24:15 +0300

    add custom button component

---
+++ src/components/button/button.component.tsx
@@ -0,0 +1,14 @@
+import React, { MouseEventHandler } from "react";
+
+import ButtonContainer from "./button.styled";
+
+interface ButtonProps {
+  title: string;
+  onClick: MouseEventHandler;
+}
+
+const Button: React.FC<ButtonProps> = ({ title, onClick }): JSX.Element => {
+  return <ButtonContainer onClick={onClick}>{title}</ButtonContainer>;
+};
+
+export default Button;
---
+++ src/components/button/button.styled.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const ButtonContainer = styled.div`
+  padding: 5px;
+  border-radius: 2px;
+  border-color: black;
+  border-width: 1px;
+  overflow: hidden;
+  background-color: rgb(208, 213, 219);
+`;
+
+export default ButtonContainer;
