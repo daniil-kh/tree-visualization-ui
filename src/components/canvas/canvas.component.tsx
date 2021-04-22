4a7a62d34db91d74b89e9bbd9f68efc6ab242133
daniil-kh <daniil.khmurovich@gmail.com>
2021-04-22 15:59:59 +0300

    add canvas resizing

--- src/components/canvas/canvas.component.tsx
+++ src/components/canvas/canvas.component.tsx
@@ -1,11 +1,31 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 
 type Props<T> = { draw: DrawFunction<T>; data: Array<any> };
 type DrawFunction<T> = (ctx: CanvasRenderingContext2D, element: T) => void;
 
 const Canvas: React.FC<Props<any>> = (props: Props<any>) => {
-  const { data, draw, ...rest } = props;
+  const { data, draw } = props;
   const canvasRef = useRef(null);
+  const [width, setWidth] = useState(window.innerWidth * 0.8);
+  const [height, setHeight] = useState(window.innerHeight * 0.5);
+
+  const resize = () => {
+    const canvas: HTMLCanvasElement = canvasRef.current!;
+    const context: CanvasRenderingContext2D = canvas.getContext("2d")!;
+
+    canvas.width = window.innerWidth * 0.8;
+    canvas.height = window.innerHeight * 0.5;
+    setHeight(window.innerHeight * 0.5);
+    setWidth(window.innerWidth * 0.8);
+
+    context.strokeRect(0, 0, context.canvas.width, context.canvas.height);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", resize);
+
+    return () => window.removeEventListener("resize", () => resize);
+  });
 
   useEffect(() => {
     const canvas: HTMLCanvasElement = canvasRef.current!;
@@ -22,12 +42,7 @@ const Canvas: React.FC<Props<any>> = (props: Props<any>) => {
   }, [data, draw]);
 
   return (
-    <canvas
-      ref={canvasRef}
-      width={window.innerWidth * 0.8}
-      height={window.innerHeight * 0.5}
-      {...rest}
-    >
+    <canvas ref={canvasRef} width={width} height={height}>
       Sorry, your browser is not supported
     </canvas>
   );
