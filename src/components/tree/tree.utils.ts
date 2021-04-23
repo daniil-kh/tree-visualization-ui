8087c4ca3c270c8ec8156fec4002938990601b64
daniil-kh <daniil.khmurovich@gmail.com>
2021-04-23 12:07:20 +0300

    eject adjustment calculation from getting drawable tree

--- src/components/tree/tree.utils.ts
+++ src/components/tree/tree.utils.ts
@@ -8,10 +8,6 @@ export const getDrawableTree = (
   let nodes: Array<RawDrawableNode<number>> = tree.getNodes();
   if (nodes.length <= 0) return [];
 
-  let widthAdjustment =
-    (window.innerWidth * 0.8 - (nodes[nodes.length - 1].x - nodes[0].x)) / 2;
-  tree.xAdjustment = widthAdjustment;
-  nodes = tree.getNodes();
   return nodes.map(
     (node): DrawableNode<number> => ({ ...node, shape: "circle", fill: false })
   );
@@ -19,17 +15,18 @@ export const getDrawableTree = (
 
 export const drawBNode = <T>(
   ctx: CanvasRenderingContext2D,
-  node: DrawableNode<T>
+  node: DrawableNode<T>,
+  adj: number
 ) => {
   let circleRadius = 20;
   ctx.beginPath();
   ctx.strokeStyle = node.color ? node.color : "#000000";
-  ctx.arc(node.x, node.y, circleRadius, 0, 2 * Math.PI);
+  ctx.arc(node.x + adj, node.y, circleRadius, 0, 2 * Math.PI);
   ctx.stroke();
   //ctx.strokeStyle = "#000000";
   ctx.font = "20px Arial";
   ctx.textAlign = "center";
-  ctx.fillText((node.data as unknown) as string, node.x, node.y + 7);
+  ctx.fillText((node.data as unknown) as string, node.x + adj, node.y + 7);
   if (node.parent) {
     const hypotenuse = Math.sqrt(
       Math.pow(node.x - node.parent.x, 2) + Math.pow(node.y - node.parent.y, 2)
@@ -40,10 +37,10 @@ export const drawBNode = <T>(
     const Y: number = Math.sqrt(Math.pow(circleRadius, 2) - Math.pow(X, 2));
 
     ctx.moveTo(
-      node.parent.x + (node.x > node.parent.x ? X : -X),
+      node.parent.x + adj + (node.x > node.parent.x ? X : -X),
       node.parent.y + Y
     );
-    ctx.lineTo(node.x + (node.x > node.parent.x ? -X : X), node.y - Y);
+    ctx.lineTo(node.x + adj + (node.x > node.parent.x ? -X : X), node.y - Y);
     ctx.stroke();
   }
   ctx.strokeStyle = "#000000";
